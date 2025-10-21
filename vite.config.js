5e335e84429aad459e3d24c991832352d764d42f
Jared Mejia <jaredmejia4440@gmail.com>
2025-10-21 16:17:55 -0600

    feat: add react compiler

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
